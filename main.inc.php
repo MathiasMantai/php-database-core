e5d1940032580fc2629d6769e5e8219c9a869254
Mantai59 <mmantaibusiness@gmail.com>
2022-05-06 12:51:55 +0200

    added main.inc.php

---
+++ main.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+# CONSTANTS
+define("PREFIX", "db_");#
+define("DATABASE","db_database");
+define("ENTRYTABLE", "db_entries");
+define("DBHOST","localhost");
+define("DBUSER","root");
+define("DBPW", "");
+###
+
+
+?>
